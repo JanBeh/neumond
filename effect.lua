36aaf0bb2036111f61f047207532ebd7dc42bed1
Jan Behrens <jbe@magnetkern.de>
2024-04-23 14:12:31 +0200

    Dedicated xpcall_traceback function in effect mod

--- effect.lua
+++ effect.lua
@@ -90,6 +90,10 @@ local function assert_nopos(success, ...)
   error(..., 0)
 end
 
+local function xpcall_traceback(func, ...)
+  return xpcall(func, debug_traceback, ...)
+end
+
 -- handle(handlers, action, ...) runs action(...) under the context of an
 -- effect handler and returns the return value of the action function (possibly
 -- modified by effect handlers).
@@ -103,10 +107,11 @@ end
 -- handler has returned, use the handle_once function instead.
 --
 function _M.handle(handlers, action, ...)
-  -- The action function gets wrapped by the xpcall function to ensure that
-  -- errors will contain a stack trace. The arguments to xpcall (including the
-  -- action function) are passed on the first "resume".
-  local action_thread = coroutine_create(xpcall)
+  -- The action function gets wrapped by the xpcall_traceback function to
+  -- ensure that errors will contain a stack trace. The arguments to
+  -- xpcall_traceback (including the action function) are passed on the first
+  -- "resume".
+  local action_thread = coroutine_create(xpcall_traceback)
   -- Ensure that coroutine's to-be-closed variables will be closed on return:
   local coro_cleaner <close> = setmetatable(
     {thread = action_thread}, coro_cleaner_metatbl
@@ -115,7 +120,8 @@ function _M.handle(handlers, action, ...)
   -- values:
   local process_action_results
   -- Function resuming the coroutine. On first invocation, the arguments to
-  -- xpcall (including the action function) must be passed as first argument.
+  -- xpcall_traceback (including the action function) must be passed as first
+  -- argument.
   local function resume(...)
     -- Pass all arguments to coroutine.resume and pass all results to
     -- process_action_results function as defined below:
@@ -124,8 +130,8 @@ function _M.handle(handlers, action, ...)
   -- Implementation for local variable process_action_results defined above:
   function process_action_results(coro_success, ...)
     -- Check if the coroutine threw an exception (should never happen as it's
-    -- supposed to be caught by the xpcall function that has been passed to
-    -- coroutine.create):
+    -- supposed to be caught by the xpcall_traceback function that has been
+    -- passed to coroutine.create):
     if coro_success then
       -- There was no exception caught.
       -- Check if coroutine finished exection:
@@ -176,8 +182,8 @@ function _M.handle(handlers, action, ...)
     end
   end
   -- Invoke the resume function for the first time, passing the arguments for
-  -- xpcall:
-  return resume(action, debug_traceback, ...)
+  -- xpcall_traceback:
+  return resume(action, ...)
 end
 
 -- Ephemeron that allows obtaining the coroutine behind a resume function that
@@ -274,11 +280,11 @@ function _M.handle_once(handlers, action, ...)
     return resume(...)
   else
     -- action is not a previously returned resume function.
-    -- Create new coroutine with xpcall:
-    action_thread = coroutine_create(xpcall)
+    -- Create new coroutine with xpcall_traceback:
+    action_thread = coroutine_create(xpcall_traceback)
     -- Use resume function to start coroutine for the first time, in which case
-    -- the action and debug.traceback needs to be passed as first arguments:
-    return resume(action, debug_traceback, ...)
+    -- the action needs to be passed as first argument:
+    return resume(action, ...)
   end
 end
 
