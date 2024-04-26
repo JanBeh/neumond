c291861c4a457e181fc99e571b1c87126676644a
Jan Behrens <jbe@magnetkern.de>
2024-04-27 00:58:01 +0200

    Added resume_later example

---
+++ examples/effect/resume_later.lua
@@ -0,0 +1,18 @@
+local effect = require "effect"
+
+local interrupt = effect.new("interrupt")
+
+local resume = effect.handle(
+  {
+    [interrupt] = function(resume)
+      return resume:persistent()
+    end,
+  },
+  function()
+    print("1")
+    interrupt()
+    print("3")
+  end
+)
+print("2")
+resume()
