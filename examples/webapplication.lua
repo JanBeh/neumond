f6fbef4cfa8a120998a0b59b98588243eb328b08
Jan Behrens <jbe@magnetkern.de>
2024-05-27 09:23:22 +0200

    Fixed web application example (no pipelining)

--- examples/webapplication.lua
+++ examples/webapplication.lua
@@ -11,30 +11,28 @@ local scgi_path = assert(..., "no socket path given")
 local function request_handler(req)
   req:process_request_body()
   local dbconn = assert(pgeff.connect("dbname=demoapplication"))
-  assert(dbconn:query("BEGIN")()) -- TODO: use pipelining better
   local result = assert(dbconn:query([[
 CREATE TABLE IF NOT EXISTS account (
   id SERIAL8 PRIMARY KEY,
-  name TEXT NOT NULL );]])());
+  name TEXT NOT NULL );]]));
   if req.post_params.action == "add" then
     local result = assert(dbconn:query(
       "INSERT INTO account (name) VALUES ($1)", req.post_params.name
-    )())
+    ))
   end
   if req.post_params.action == "delete" then
     for key, value in pairs(req.post_params) do
       local id = tonumber((string.match(key, "^id([0-9]+)$")))
       if id then
         local result = assert(
-          dbconn:query("DELETE FROM account WHERE id=$1", id)()
+          dbconn:query("DELETE FROM account WHERE id=$1", id)
         )
       end
     end
   end
   local result = assert(
-    dbconn:query("SELECT * FROM account ORDER BY name, id")()
+    dbconn:query("SELECT * FROM account ORDER BY name, id")
   )
-  assert(dbconn:query("COMMIT")()) -- TODO: use pipelining better
   req:write("Content-type: text/html\n\n")
   req:write('<html><head><title>demoapplication</title></head><body>\n')
   req:write('<h1>Accounts</h1>\n')
