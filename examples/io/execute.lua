f6e1df414553c23253a17726a1feba36f6fc8cdf
Jan Behrens <jbe@magnetkern.de>
2024-05-31 20:54:57 +0200

    Fixed eio.execute example regarding stdout/stderr

--- examples/io/execute.lua
+++ examples/io/execute.lua
@@ -2,30 +2,36 @@ local fiber = require "fiber"
 local waitio_fiber = require "waitio_fiber"
 local eio = require "eio"
 
+local function read_background(stream, maxlen)
+  return fiber.spawn(function()
+    local output = assert(stream:read(maxlen))
+    if assert(stream:read(1)) ~= "" then
+      error("too much data from child process")
+    end
+    return output
+  end)
+end
+
 local function shell_add(a, b)
   local a = assert(tonumber(a))
   local b = assert(tonumber(b))
   local proc = assert(eio.execute("sh", "-c", "echo $(("..a.."+"..b.."))"))
-  local stdout_fiber = fiber.spawn(function()
-    return assert(proc.stdout:read())
-  end)
-  local stderr_fiber = fiber.spawn(function()
-    return assert(proc.stderr:read())
-  end)
-  local stdout = stdout_fiber:await()
-  local stderr = stderr_fiber:await()
-  local retval = proc:wait()
-  if retval ~= 0 then
-    print("Standard error output of subprocess:")
-    io.stdout:write(stderr)
-    print("---")
-    if retval < 0 then
-      error("Process terminated due to signal " .. -retval .. ".")
-    else
-      error("Process exited with exit code " .. retval .. ".")
+  return fiber.scope(function()
+    local stdout_fiber = read_background(proc.stdout, 1024)
+    local stderr_fiber = read_background(proc.stderr, 1024*1024)
+    local retval = proc:wait()
+    if retval ~= 0 then
+      print("Standard error output of subprocess:")
+      io.stdout:write(stderr_fiber:await())
+      print("---")
+      if retval < 0 then
+        error("Process terminated due to signal " .. -retval .. ".")
+      else
+        error("Process exited with exit code " .. retval .. ".")
+      end
     end
-  end
-  return assert(tonumber(stdout))
+    return assert(tonumber(stdout_fiber:await()))
+  end)
 end
 
 
