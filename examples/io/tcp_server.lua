2f4bc17296b3e5c0114f4810d0124aadef93a326
Jan Behrens <jbe@magnetkern.de>
2024-07-06 15:47:01 +0200

    Correctly handle I/O errors in TCP server example

--- examples/io/tcp_server.lua
+++ examples/io/tcp_server.lua
@@ -4,6 +4,26 @@ local runtime = require "neumond.runtime"
 local eio = require "neumond.eio"
 
 local terminate = effect.new("terminate")
+local timeout = effect.new("timeout")
+local client_ioerr = effect.new("client_ioerr")
+
+local function assert_io(first, ...)
+  if first then
+    return first, ...
+  else
+    client_ioerr(...)
+  end
+end
+
+local client_ioerr_handlers = {
+  [client_ioerr] = function(resume, message)
+    print("Client I/O error: " .. tostring(message))
+  end,
+}
+
+local function handle_client_ioerr(...)
+  return fiber.handle(client_ioerr_handlers, ...)
+end
 
 local function main(...)
   return fiber.handle(
@@ -17,17 +37,25 @@ local function main(...)
       fiber.spawn(function() eio.catch_signal(15)(); terminate() end)
       local listener = assert(eio.tcplisten(nil, 1234))
       while true do
-        local conn = listener:accept()
-        fiber.spawn(function()
-          conn:flush("Hello World!\n")
-          conn:shutdown()
-          local line = assert(conn:read(1024, "\n")):match("[^\r\n]*")
-          if line == "" then
-            print("Got empty request.")
+        local conn = assert(listener:accept())
+        fiber.spawn(handle_client_ioerr, function()
+          local conn <close> = conn
+          assert_io(conn:flush("Hello, what is your name?\n"))
+          local name =
+            assert_io(conn:read(1024, "\n")):match("^[ \t]*(.-)\r?\n?$")
+          if name == "" then
+            assert_io(conn:flush("You didn't provide a name, bye.\n"))
           else
-            print("Got request: " .. line)
+            assert_io(conn:flush("Hello " .. name .. "!\n"))
+            assert_io(conn:flush("You may send some final data now.\n"))
+            assert_io(conn:shutdown())
+            local data = assert_io(conn:read(1024))
+            if data == "" then
+              print("Got no data.")
+            else
+              print("Got some data: " .. data)
+            end
           end
-          conn:close()
         end)
       end
     end
