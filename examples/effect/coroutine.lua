e5cf3cfdf3700956806e2896786993aab04d1052
Jan Behrens <jbe@magnetkern.de>
2025-08-05 16:16:03 +0200

    Added finalizer run to coroutine example

--- examples/effect/coroutine.lua
+++ examples/effect/coroutine.lua
@@ -31,45 +31,75 @@ function create_coro(action)
     end
   )
 
-  -- Return function that resumes the coroutine:
-  return function(...)
+  -- Return callable table that resumes the coroutine when called,
+  -- but which can also be closed, such that finalizers are run:
+  return setmetatable({}, {
 
-    -- If no continuation is stored, then the coroutine is running,
-    -- has failed, or has terminated successfully:
-    if stored_resume == nil then
-      error("cannot resume coroutine", 2)
-    end
+    -- Function invoked when handle is called:
+    __call = function(self, ...)
+      -- If no continuation is stored, then the coroutine is running,
+      -- has failed, has been closed, or has terminated successfully:
+      if not stored_resume then
+        error("cannot resume coroutine", 2)
+      end
 
-    -- Get continuation and store nil as continuation:
-    local resume = stored_resume
-    stored_resume = nil
+      -- Get continuation and store nil as continuation:
+      local resume = stored_resume
+      stored_resume = nil
 
-    -- Resume coroutine by calling continuation with given arguments
-    -- and returning any return values:
-    return resume(...)
+      -- Resume coroutine by calling continuation with given arguments
+      -- and returning any return values:
+      return resume(...)
+    end,
 
-  end
+    -- Function invoked when handle is closed
+    -- (using <close> variable):
+    __close = function(self)
+      -- Check if continuation is stored:
+      if stored_resume then
+        -- Discontinue continuation (runs finalizers):
+        stored_resume:discontinue()
+        -- Removed stored continuation:
+        stored_resume = nil
+      end
+    end,
+
+  })
 
 end
 
-local coro = create_coro(function(arg)
-  assert(arg == "start arg")
-  print(1)
-  local v = yield("A")
-  assert(v == "resume arg")
-  print(3)
-  local v = yield("B")
-  assert(v == "another resume arg")
-  print(5)
-  return "done"
-end)
+do
+  local coro <close> = create_coro(function(arg)
+    assert(arg == "start arg")
+    print(1)
+    local v = yield("A")
+    assert(v == "resume arg")
+    print(3)
+    local v = yield("B")
+    assert(v == "another resume arg")
+    print(5)
+    return "done"
+  end)
 
-local v = coro("start arg")
-assert(v == "A")
-print("2")
-local v = coro("resume arg")
-assert(v == "B")
-print("4")
-local v = coro("another resume arg")
-assert (v == "done")
---coro() -- would result in: cannot resume coroutine
+  local v = coro("start arg")
+  assert(v == "A")
+  print("2")
+  local v = coro("resume arg")
+  assert(v == "B")
+  print("4")
+  local v = coro("another resume arg")
+  assert (v == "done")
+  --coro() -- would result in: cannot resume coroutine
+end
+
+do
+  local coro <close> = create_coro(function(arg)
+    local guard <close> = setmetatable({}, { __close = function()
+      print("Cleanup")
+    end })
+    yield("X")
+    error("should not be reached")
+  end)
+  assert(coro() == "X")
+  -- ending the block will print "Cleanup"
+end
